2fed8796ad24055def5339a1bf323af0acff1998
msjonatan <msjonatan@outlook.com>
2023-04-19 00:39:56 -0600

    Second Commit

--- src/main/groovy/com/mpc/spellcaster/Context.groovy
+++ src/main/groovy/com/mpc/spellcaster/Context.groovy
@@ -1,9 +1,14 @@
 package com.mpc.spellcaster
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import groovy.transform.ToString
 
+@ToString(includeNames=true, includePackage=false)
+@JsonSerialize
 class Context implements Serializable {
     Map<String, Object> contextMap = [:]
+    Map<String, Object> variables = [:]
 
     String toJson() {
         new ObjectMapper().writeValueAsString(contextMap)
--- src/main/groovy/com/mpc/spellcaster/service/SpellCasterService.groovy
+++ src/main/groovy/com/mpc/spellcaster/service/SpellCasterService.groovy
@@ -1,14 +1,32 @@
 package com.mpc.spellcaster.service
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.mpc.spellcaster.Context
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.expression.Expression
+import org.springframework.expression.spel.standard.SpelExpressionParser
 import org.springframework.stereotype.Service
+import redis.clients.jedis.Jedis
+import redis.clients.jedis.JedisPool
+import redis.clients.jedis.JedisPoolConfig
 
 @Service
 class SpellCasterService {
-    @Autowired
-    RedisTemplate<String, Context> redisTemplate
+    JedisPool jedisPool = new JedisPool(new JedisPoolConfig(), "localhost")
 
-    // ...
+    def evaluate(String expression, Context context) {
+        try (Jedis jedis = jedisPool.getResource()) {
+            // Store the context object in Redis
+            jedis.set("context", new ObjectMapper().writeValueAsString(context))
+
+            // Evaluate the expression using SpEL
+            final SpelExpressionParser parser = new SpelExpressionParser()
+            final Expression exp = parser.parseExpression(expression)
+            final Object result = exp.getValue(context)
+
+            // Retrieve the updated context object from Redis
+            final Context updatedContext = new ObjectMapper().readValue(jedis.get("context"), Context.class)
+
+            return [result: result, context: updatedContext]
+        }
+    }
 }
